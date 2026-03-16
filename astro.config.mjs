e52322ecda4a69a8571a84c8ecc38ed5bed16b20
talison-cardoso <talisong.me@gmail.com>
2026-03-16 01:48:13 -0300

    fix: returns to default configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  extends: ['plugin:astro/recommended', 'plugin:prettier/recommended'],
-  overrides: [
-    {
-      files: ['*.astro'],
-      parser: 'astro-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro'],
-      },
-    },
-  ],
-});
+export default defineConfig({});
